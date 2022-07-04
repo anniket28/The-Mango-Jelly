8895a87e6db193fcf0491fbd760e2e119463b981
anniket28 <74172788+anniket28@users.noreply.github.com>
2022-07-04 19:41:42 +0530

    commit

--- routers/Crud.js
+++ routers/Crud.js
@@ -9,7 +9,7 @@ const router=express.Router()
 router.post('/add-mobile',async(req,res)=>{
     try {
         let mobile=await Mobile.findOne({name:req.body.name})
-
+        
         // Checking if mobile exists
         if(mobile){
             res.json({exists:true,"Mobile Already Exists":"Mobile with this name already exists."})
@@ -49,8 +49,8 @@ router.post('/edit-mobile',async(req,res)=>{
             if(req.body.description){newData.description=req.body.description}
             if(req.body.brandName){newData.brandName=req.body.brandName}
             if(req.body.ram){newData.ram=req.body.ram}
-            if(req.body.internalStorage){newData.ram=req.body.internalStorage}
-            if(req.body.screenSize){newData.ram=req.body.screenSize}
+            if(req.body.internalStorage){newData.internalStorage=req.body.internalStorage}
+            if(req.body.screenSize){newData.screenSize=req.body.screenSize}
 
             // Updating Data
             const editMobile=await Mobile.findByIdAndUpdate(mobile._id,{$set:newData},{new:true})
